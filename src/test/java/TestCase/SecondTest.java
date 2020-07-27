1b7e0a0c2d706756b1178c6bc3fff29684e28711
Jeferson Lopes Eugenio <jefyx1@hotmail.com>
2020-07-27 11:23:48 -0300

    segundo teste api

---
+++ src/test/java/TestCase/SecondTest.java
@@ -0,0 +1,53 @@
+package TestCase;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SecondTest {
+
+    private static RequestSpecification requestSpec;
+    private static ResponseSpecification responseSpec;
+
+    @BeforeEach
+    public void setUp() {
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://api-de-tarefas.herokuapp.com")
+                .setBasePath("/contacts")
+                .addHeader("Content-Type","application/json")
+                .addHeader("Accept","application/vnd.tasksmanager.v2")
+                .build();
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+    }
+    
+    @Test
+    public void listarContatosId46StatusCode200Possitivo() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/46")
+                .then()
+                .log().body()
+                .spec(responseSpec)
+                .statusCode(200);
+    }
+    @Test
+    public void listarContatosStatusCode400Negativo() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get()
+                .then()
+                .spec(responseSpec)
+                .statusCode(400);
+    }
+
+}
