54b39e3d1675c3163d9a87842e7becfda8e7f9f8
jefynhu11 <jefyx1@hotmail.com>
2020-07-28 16:26:00 -0300

    Coloquei .log().body()

--- src/test/java/TestCase/SecondTest.java
+++ src/test/java/TestCase/SecondTest.java
@@ -35,6 +35,7 @@ public class SecondTest {
                 .when()
                 .get()
                 .then()
+                .log().body()
                 .spec(responseSpec)
                 .statusCode(200);
     }
