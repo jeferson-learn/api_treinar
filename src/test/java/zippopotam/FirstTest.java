2da1b005e263f279e2261dba78b0ed05b92b7456
jefynhu11 <jefyx1@hotmail.com>
2020-07-30 09:57:11 -0300

    Inicia teste api zip

---
+++ src/test/java/zippopotam/FirstTest.java
@@ -0,0 +1,39 @@
+package zippopotam;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class FirstTest {
+
+    private static RequestSpecification requestSpecification;
+    private static ResponseSpecification responseSpecification;
+
+    @Test
+    public void teste(){
+        requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("http://api.zippopotam.us")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Accept", "application/vnd.tasksmanager.v2")
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(200)
+                .build();
+
+        given()
+                .spec(requestSpecification)
+        .when()
+                .get()
+        .then()
+                .log().body()
+                .spec(responseSpecification);
+    }
+
+}
