014b4159dbe74041963a9e233d9f08906180817b
jefynhu11 <jefyx1@hotmail.com>
2020-08-04 16:27:32 -0300

    Teste contrato com XML

---
+++ src/test/java/TesteXML/FirstTest.java
@@ -0,0 +1,60 @@
+package TesteXML;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+public class FirstTest {
+
+    private String id;
+    private static RequestSpecification requestSpec;
+    private static ResponseSpecification responseSpec;
+
+    @BeforeEach
+    public void setUp() {
+
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("https://restapi.wcaquino.me/usersXML")
+                .build();
+
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+
+    }
+
+    @Test
+    public void criarContatoComObjetoXml(){
+        id = given()
+                .spec(requestSpec)
+            .when()
+                .post()
+            .then()
+                .log().body()
+                .spec(responseSpec)
+//                .body(.matchesJsonSchemaInClasspath("Schemas" + File.separator + "XmlSchema.xsd"))
+                .and()
+                .extract().path("data.id");
+
+        deleteContato();
+    }
+
+    public void deleteContato(){
+        given()
+                .spec(requestSpec)
+        .when()
+                .delete("/"+id)
+        .then()
+                .statusCode(204);
+    }
+
+}
