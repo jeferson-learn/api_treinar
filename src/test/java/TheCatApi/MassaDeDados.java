0242424b0224d1eb5deef41bf7c7db1cf83c7512
jefynhu11 <jefyx1@hotmail.com>
2020-08-04 16:33:48 -0300

    Codigo limpo e extends para classe Cadastro

---
+++ src/test/java/TheCatApi/MassaDeDados.java
@@ -0,0 +1,11 @@
+package TheCatApi;
+
+public class MassaDeDados {
+
+    String vote_id;
+    String urlCadastro = "user/passwordlesssignup";
+    String corpoCadastro = "{\"email\": \"jefynhuwork@gmail.com\", \"appDescription\": \"The Cat Api\"}";
+    String votacao = "{\"image_id\": \"btt\", \"value\": \"true\", \"sub_id\": \"demo-4b9e47\"}";
+    String corpoFavorita = "{\"image_id\": \"bkg\"}";
+
+}
